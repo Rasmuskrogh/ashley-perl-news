2b202897a68f037d626c38c7b7c259cc597c7b81
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2024-05-08 21:43:09 +0200

    build the example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1>MEIN NAME IST ASHLEY PERL UND ICH LIEBE PALATSCHINKEN!</h1>
     </>
   );
---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+
+const header = () => {
+  return (
+    <header className="main-header">
+      <h1>ASHLEY PERL</h1>
+      <ul>
+        <li>ABOUT</li>
+        <li>WRITING</li>
+        <li>CONTACT</li>
+      </ul>
+    </header>
+  );
+};
+
+export default header;
